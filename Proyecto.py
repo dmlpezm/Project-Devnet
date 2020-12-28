1a33460d05ab7d4fa931ce991c5cf773f07e2c0c
David Lopez <mateolpez190@gmail.com>
2020-12-28 17:19:56 -0500

    Get token from DNA

--- Proyecto.py
+++ Proyecto.py
@@ -2,15 +2,30 @@ import pyotp
 import sqlite3
 import hashlib
 import uuid
-from flask import Flask, request
+import requests
+import json
+from flask import Flask
 
 app=Flask(__name__)
 
-
-@app.route('/welcome')
+@app.route('/')
 def index():
     return 'Welcome David Lopez APP'
 
+@app.route('/permiso')
+def lista():
+    url = "https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token"
+    headers = {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+    }
+    respuesta = requests.post(url, auth=('devnetuser', 'Cisco123!'), headers=headers)
+    token = respuesta.text
+    token = json.loads(token)
+    token = token['Token']
+    return token
+
+
 if __name__ == '__main__':
-    app.run(host='sandboxdnac.cisco.com', ssl_context='adhoc')
+    app.run()
 
