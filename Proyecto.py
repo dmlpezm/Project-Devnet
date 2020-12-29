0499de033c6e103db729c81b69390e6f2e177c15
David Lopez <mateolpez190@gmail.com>
2020-12-28 22:59:40 -0500

    Get devices from DNA + Token

--- Proyecto.py
+++ Proyecto.py
@@ -4,7 +4,8 @@ import hashlib
 import uuid
 import requests
 import json
-from flask import Flask
+from flask import Flask, request, jsonify
+from llave import token
 
 app=Flask(__name__)
 
@@ -12,20 +13,20 @@ app=Flask(__name__)
 def index():
     return 'Welcome David Lopez APP'
 
-@app.route('/permiso')
-def lista():
-    url = "https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token"
+
+@app.route('/dispositivos', methods=['GET','POST'])
+def listar():
+    url = 'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device'
     headers = {
-        'Content-type': 'application/json',
-        'Accept': 'application/json'
+        'Accept': 'application/json',
+        'X-Auth-Token': token
     }
-    respuesta = requests.post(url, auth=('devnetuser', 'Cisco123!'), headers=headers)
-    token = respuesta.text
-    token = json.loads(token)
-    token = token['Token']
-    return token
+    respuesta = requests.request('GET', url, headers=headers, verify=False)
+    resultado = respuesta.text
+    resultado = json.loads(resultado)
+    return jsonify(resultado)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
---
+++ llave.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+url = "https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token"
+headers = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json'
+}
+respuesta = requests.post(url, auth=(
+    'devnetuser', 'Cisco123!'), headers=headers)
+token = respuesta.text
+token = json.loads(token)
+token = token['Token']
